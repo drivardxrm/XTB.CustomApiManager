398d51f4c4fb490f5df17ebab9d168f247a3b5a6
drivardxrm <38399134+drivardxrm@users.noreply.github.com>
2020-11-18 23:02:50 -0500

    custom api fields

--- XTB.CustomApiManager/CustomApiManagerControl.designer.cs
+++ XTB.CustomApiManager/CustomApiManagerControl.designer.cs
@@ -45,6 +45,23 @@
             this.chkUnmanaged = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
             this.cmbSolution = new System.Windows.Forms.ComboBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.chkIsFunction = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.chkIsPrivate = new System.Windows.Forms.CheckBox();
+            this.txtUniqueName = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cboBindingType = new System.Windows.Forms.ComboBox();
+            this.cboAllowedCustomProcessingStep = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtDisplayName = new System.Windows.Forms.TextBox();
             this.toolStripMenu.SuspendLayout();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.gridCustomApiResponseProperty)).BeginInit();
@@ -99,8 +116,6 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.gridCustomApiResponseProperty);
-            this.groupBox1.Controls.Add(this.gridCustomApiRequestParameter);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.gridCustomApi);
             this.groupBox1.Controls.Add(this.chkUnmanaged);
@@ -118,7 +133,7 @@
             this.gridCustomApiResponseProperty.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.gridCustomApiResponseProperty.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.dataGridViewTextBoxColumn2});
-            this.gridCustomApiResponseProperty.Location = new System.Drawing.Point(10, 433);
+            this.gridCustomApiResponseProperty.Location = new System.Drawing.Point(435, 435);
             this.gridCustomApiResponseProperty.Name = "gridCustomApiResponseProperty";
             this.gridCustomApiResponseProperty.Size = new System.Drawing.Size(240, 150);
             this.gridCustomApiResponseProperty.TabIndex = 7;
@@ -135,7 +150,7 @@
             this.gridCustomApiRequestParameter.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.gridCustomApiRequestParameter.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.dataGridViewTextBoxColumn1});
-            this.gridCustomApiRequestParameter.Location = new System.Drawing.Point(10, 267);
+            this.gridCustomApiRequestParameter.Location = new System.Drawing.Point(435, 279);
             this.gridCustomApiRequestParameter.Name = "gridCustomApiRequestParameter";
             this.gridCustomApiRequestParameter.Size = new System.Drawing.Size(240, 150);
             this.gridCustomApiRequestParameter.TabIndex = 6;
@@ -202,11 +217,173 @@
             this.cmbSolution.TabIndex = 0;
             this.cmbSolution.SelectedIndexChanged += new System.EventHandler(this.cmbSolution_SelectedIndexChanged);
             // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(532, 132);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(359, 20);
+            this.txtDescription.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(454, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Description";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(454, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(532, 80);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(359, 20);
+            this.txtName.TabIndex = 9;
+            // 
+            // chkIsFunction
+            // 
+            this.chkIsFunction.AutoSize = true;
+            this.chkIsFunction.Location = new System.Drawing.Point(532, 158);
+            this.chkIsFunction.Name = "chkIsFunction";
+            this.chkIsFunction.Size = new System.Drawing.Size(15, 14);
+            this.chkIsFunction.TabIndex = 11;
+            this.chkIsFunction.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(454, 158);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "IsFunction";
+            // 
+            // chkIsPrivate
+            // 
+            this.chkIsPrivate.AutoSize = true;
+            this.chkIsPrivate.Location = new System.Drawing.Point(532, 186);
+            this.chkIsPrivate.Name = "chkIsPrivate";
+            this.chkIsPrivate.Size = new System.Drawing.Size(15, 14);
+            this.chkIsPrivate.TabIndex = 13;
+            this.chkIsPrivate.UseVisualStyleBackColor = true;
+            // 
+            // txtUniqueName
+            // 
+            this.txtUniqueName.Location = new System.Drawing.Point(532, 54);
+            this.txtUniqueName.Name = "txtUniqueName";
+            this.txtUniqueName.Size = new System.Drawing.Size(359, 20);
+            this.txtUniqueName.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(454, 57);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Unique Name";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(454, 186);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "IsPrivate";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(788, 279);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // cboBindingType
+            // 
+            this.cboBindingType.FormattingEnabled = true;
+            this.cboBindingType.Location = new System.Drawing.Point(532, 206);
+            this.cboBindingType.Name = "cboBindingType";
+            this.cboBindingType.Size = new System.Drawing.Size(180, 21);
+            this.cboBindingType.TabIndex = 6;
+            // 
+            // cboAllowedCustomProcessingStep
+            // 
+            this.cboAllowedCustomProcessingStep.FormattingEnabled = true;
+            this.cboAllowedCustomProcessingStep.Location = new System.Drawing.Point(649, 233);
+            this.cboAllowedCustomProcessingStep.Name = "cboAllowedCustomProcessingStep";
+            this.cboAllowedCustomProcessingStep.Size = new System.Drawing.Size(180, 21);
+            this.cboAllowedCustomProcessingStep.TabIndex = 18;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(454, 209);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(65, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Binding type";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(454, 236);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(189, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Allowed Custom Processing Step Type";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(454, 109);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(72, 13);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Display Name";
+            // 
+            // txtDisplayName
+            // 
+            this.txtDisplayName.Location = new System.Drawing.Point(532, 106);
+            this.txtDisplayName.Name = "txtDisplayName";
+            this.txtDisplayName.Size = new System.Drawing.Size(359, 20);
+            this.txtDisplayName.TabIndex = 21;
+            // 
             // CustomApiManagerControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtDisplayName);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.cboAllowedCustomProcessingStep);
+            this.Controls.Add(this.cboBindingType);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.gridCustomApiResponseProperty);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.gridCustomApiRequestParameter);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtUniqueName);
+            this.Controls.Add(this.chkIsPrivate);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.chkIsFunction);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDescription);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.toolStripMenu);
             this.Name = "CustomApiManagerControl";
@@ -241,5 +418,22 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
         private System.Windows.Forms.DataGridView gridCustomApiRequestParameter;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.CheckBox chkIsFunction;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox chkIsPrivate;
+        private System.Windows.Forms.TextBox txtUniqueName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cboBindingType;
+        private System.Windows.Forms.ComboBox cboAllowedCustomProcessingStep;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtDisplayName;
     }
 }
